bc57c77a5419b8d445fc4c579b8e3916b7d0c83c
JeanAkaPascal <33148890+JeanAkaPascal@users.noreply.github.com>
2019-01-24 16:17:57 +0100

    JWT

--- dbconnection.js
+++ dbconnection.js
@@ -3,8 +3,8 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
-  database: "homestead"
+  password: "banana",
+  database: "webproject"
 });
 /*connection.connect(function(err){
   if(err){
