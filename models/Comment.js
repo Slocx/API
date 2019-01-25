c0bbd147e8e19fd51307ce8778e6b76bb936cb4d
Slocx <33150302+Slocx@users.noreply.github.com>
2019-01-25 09:22:12 +0100

    Category Check GET(ID/ALL)/POST/PUT/DELETE
    +delete files .jade useless

--- models/Comment.js
+++ models/Comment.js
@@ -1,19 +1,19 @@
 var db = require('../dbconnection'); //reference of dbconnection.js  
 var Comment = {  
     getAllComments: function(callback) {  
-        return db.query("Select * from comment", callback);  
+        return db.query("Select * from Comment", callback);  
     },  
     getCommentsById: function(id, callback) {  
-        return db.query("select * from comment where id_comment=?", [id], callback);  
+        return db.query("select * from Comment where id_Comment=?", [id], callback);  
     },  
     addComment: function(Comment, callback) {  
-        return db.query("Insert into comment(comment_body) values(?)", [comment.comment_body], callback);  
+        return db.query("Insert into Comment(Comment_body) values(?)", [Comment.Comment_body], callback);  
     },  
     deleteComment: function(id, callback) {  
-        return db.query("delete from comment where id_comment=?", [id], callback);  
+        return db.query("delete from Comment where id_Comment=?", [id], callback);  
     },  
     updateComment: function(id, Comment, callback) {  
-        return db.query("update comment set comment_body=? where id_comment=?", [comment.comment_body,id], callback);  
+        return db.query("update Comment set Comment_body=? where id_Comment=?", [Comment.Comment_body,id], callback);  
     }  
 };  
 module.exports = Comment;  
