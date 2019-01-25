05f964fa33a7121efd4102c4827b1c1dfa43b7f3
Slocx <33150302+Slocx@users.noreply.github.com>
2019-01-25 10:44:23 +0100

    Schools Check GET(Id/All)/POST/PUT/DELETE

--- models/School.js
+++ models/School.js
@@ -4,16 +4,16 @@ var School = {
         return db.query("Select * from School", callback);  
     },  
     getSchoolsById: function(id, callback) {  
-        return db.query("select * from School where id_School=?", [id], callback);  
+        return db.query("select * from School where id_school=?", [id], callback);  
     },  
     addSchool: function(School, callback) {  
-        return db.query("Insert into School(School_name) values(?)", [School.School_name], callback);  
+        return db.query("Insert into School(school_name) values(?)", [School.school_name], callback);  
     },  
     deleteSchool: function(id, callback) {  
-        return db.query("delete from School where id_School=?", [id], callback);  
+        return db.query("delete from School where id_school=?", [id], callback);  
     },  
     updateSchool: function(id, School, callback) {  
-        return db.query("update School set School_name=? where id_School=?", [School.School_name, id], callback);  
+        return db.query("update School set school_name=? where id_school=?", [School.school_name, id], callback);  
     }  
 };  
 module.exports = School;  
