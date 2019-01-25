5cf6bc094ea5db4b6b37f52342bd3eff13145033
Slocx <33150302+Slocx@users.noreply.github.com>
2019-01-25 11:00:33 +0100

    Stock Check GET(Id/All)/POST/PUT/DELETE

--- models/Stock.js
+++ models/Stock.js
@@ -4,16 +4,16 @@ var Stock = {
         return db.query("Select * from stock", callback);  
     },  
     getStocksById: function(id, callback) {  
-        return db.query("select * from stock where id_stock=?", [id], callback);  
+        return db.query("select * from Stock where id_stock=?", [id], callback);  
     },  
     addStock: function(Stock, callback) {  
-        return db.query("Insert into stock(stock_quantity) values(?)", [stock.stock_quantity, stock.id_product], callback);  
+        return db.query("Insert into Stock(stock_quantity, id_product) values(?,?)", [Stock.stock_quantity, Stock.id_product], callback);  
     },  
     deleteStock: function(id, callback) {  
-        return db.query("delete from stock where id_stock=?", [id], callback);  
+        return db.query("delete from Stock where id_stock=?", [id], callback);  
     },  
     updateStock: function(id, Stock, callback) {  
-        return db.query("update stock set stock_quantity=?,id_product=? where id_stock=?", [stock.stock_quantity, id_product, id], callback);  
+        return db.query("update Stock set stock_quantity=?,id_product=? where id_stock=?", [Stock.stock_quantity, Stock.id_product, id], callback);  
     }  
 };  
 module.exports = Stock;  
