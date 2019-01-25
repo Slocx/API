95866974235e744cba42078eebc58ec31600a34d
Slocx <33150302+Slocx@users.noreply.github.com>
2019-01-25 10:38:12 +0100

    Products Check GET(Id/All)/POST/PUT/DELETE

--- models/Product.js
+++ models/Product.js
@@ -4,16 +4,16 @@ var Product = {
         return db.query("Select * from Product", callback);  
     },  
     getProductsById: function(id, callback) {  
-        return db.query("select * from Product where id_Product=?", [id], callback);  
+        return db.query("select * from Product where id_product=?", [id], callback);  
     },  
     addProduct: function(Product, callback) {  
-        return db.query("Insert into Product(Product_name, Product_price, id_stock, id_category) values(?,?,?,?)", [Product.Product_name, Product.Product_body, Product.Product_date, Product.Product_location, Product.id_user_create, Product.id_status_Product, Product.id_user_validate], callback);  
+        return db.query("Insert into Product(product_name, product_price, id_stock, id_category) values(?,?,?,?)", [Product.product_name, Product.product_price, Product.id_stock, Product.id_category], callback);  
     },  
     deleteProduct: function(id, callback) {  
-        return db.query("delete from Product where id_Product=?", [id], callback);  
+        return db.query("delete from Product where id_product=?", [id], callback);  
     },  
     updateProduct: function(id, Product, callback) {  
-        return db.query("update Product set Product_name=?,Product_price=?,id_stock,id_category=? where id_Product=?", [Product.Product_name, Product.Product_price, id_stock, id_category, id], callback);  
+        return db.query("update Product set product_name=?,product_price=?,id_stock=?,id_category=? where id_product=?", [Product.product_name, Product.product_price, Product.id_stock, Product.id_category, id], callback);  
     }  
 };  
 module.exports = Product;  
