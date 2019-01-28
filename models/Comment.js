84981ebd214258823913d63e88c88479eea0d13b
Slocx <33150302+Slocx@users.noreply.github.com>
2019-01-28 12:25:20 +0100

    Check Comment GET(Id/All)/POST/PUT/DELETE

--- models/Comment.js
+++ models/Comment.js
@@ -7,13 +7,13 @@ var Comment = {
         return db.query("select * from Comment where id_comment=?", [id], callback);  
     },  
     addComment: function(Comment, callback) {  
-        return db.query("Insert into Comment(id_picture_event, id_user, comment_body) values(?,?,?)", [Comment.id_picture_event, Comment.id_user, Comment.comment_body], callback);  
+        return db.query("Insert into Comment(comment_body, comment_date, id_picture_event, id_user) values(?,?,?,?)", [Comment.comment_body, Comment.comment_date, Comment.id_picture_event, Comment.id_user], callback);  
     },  
     deleteComment: function(id, callback) {  
         return db.query("delete from Comment where id_comment=?", [id], callback);  
     },  
     updateComment: function(id, Comment, callback) {  
-        return db.query("update Comment set id_picture_event=?, id_user=?,comment_body=? where id_picture_event=?", [Comment.id_picture_event, Comment.id_user, Comment.comment_body,id], callback);  
+        return db.query("update Comment set comment_body=?, comment_date=?, id_picture_event=?, id_user=? where id_comment=?", [Comment.comment_body, Comment.comment_date, Comment.id_picture_event, Comment.id_user, id], callback);  
     }  
 };  
 module.exports = Comment;  
