9aa54d8073ded29ef185c3d58dcfea201e1127f9
Slocx <33150302+Slocx@users.noreply.github.com>
2019-01-28 15:14:14 +0100

    Update Check Product

--- models/Product.js
+++ models/Product.js
@@ -7,13 +7,13 @@ var Product = {
         return db.query("select * from Product where id_product=?", [id], callback);  
     },  
     addProduct: function(Product, callback) {  
-        return db.query("Insert into Product(product_name, product_price, id_stock, id_category) values(?,?,?,?)", [Product.product_name, Product.product_price, Product.id_stock, Product.id_category], callback);  
+        return db.query("Insert into Product(product_name, product_desc, product_price, stock, id_category) values(?,?,?,?,?)", [Product.product_name, Product.product_desc, Product.product_price, Product.stock, Product.id_category], callback);  
     },  
     deleteProduct: function(id, callback) {  
         return db.query("delete from Product where id_product=?", [id], callback);  
     },  
     updateProduct: function(id, Product, callback) {  
-        return db.query("update Product set product_name=?,product_price=?,id_stock=?,id_category=? where id_product=?", [Product.product_name, Product.product_price, Product.id_stock, Product.id_category, id], callback);  
+        return db.query("update Product set product_name=?,product_desc=?,product_price=?,stock=?,id_category=? where id_product=?", [Product.product_name, Product.product_desc, Product.product_price, Product.stock, Product.id_category, id], callback);  
     }  
 };  
 module.exports = Product;  
