700101dfbfc48460d894410e99a73911043bab2a
Slocx <33150302+Slocx@users.noreply.github.com>
2019-01-28 15:30:38 +0100

    Update Check User

--- models/User.js
+++ models/User.js
@@ -7,13 +7,13 @@ var User = {
         return db.query("select * from User where id_User=?", [id], callback);  
     },  
     addUser: function(User, callback) {  
-        return db.query("Insert into User(first_name, last_name, email, password, id_status_user, id_school) values(?,?,?,?,?,?)", [User.first_name, User.last_name, User.email, User.password, User.id_status_user, User.id_school], callback);  
+        return db.query("Insert into User(first_name, last_name, email, password, profile_pic, token, id_status_user, id_school) values(?,?,?,?,?,?,?,?)", [User.first_name, User.last_name, User.email, User.password, User.profile_pic, User.token, User.id_status_user, User.id_school], callback);  
     },
     deleteUser: function(id, callback) {  
         return db.query("delete from User where id_User=?", [id], callback);  
     },  
     updateUser: function(id, User, callback) {  
-        return db.query("update User set first_name=?,last_name=?,email=?,password=?,id_status_user=?,id_school=? where id_user=?", [User.first_name, User.last_name, User.email, User.password, User.id_status_user, User.id_school, id], callback);  
+        return db.query("update User set first_name=?,last_name=?,email=?,password=?,profile_pic=?,token=?,id_status_user=?,id_school=? where id_user=?", [User.first_name, User.last_name, User.email, User.password, User.profile_pic, User.token, User.id_status_user, User.id_school, id], callback);  
     }  
 };  
 module.exports = User;  
