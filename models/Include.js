b5e54bc5c0c4e8c991f77f6e616c8fdc590600d8
Slocx <33150302+Slocx@users.noreply.github.com>
2019-01-29 09:51:59 +0100

    Try check Include order

--- models/Include.js
+++ models/Include.js
@@ -4,16 +4,16 @@ var Include = {
         return db.query("Select * from Include", callback);  
     },  
     getIncludesById: function(id, callback) {  
-        return db.query("select * from Include where id_order=?", [id], callback);  
+        return db.query("select * from Include where id_order=? && id_product=?", [id, id], callback);  
     },  
     addInclude: function(Include, callback) {  
-        return db.query("Insert into Include(quantity) values(?)", [Include.quantity], callback);  
+        return db.query("Insert into Include(id_order, id_product, quantity) values(?,?,?)", [Include.id_order, Include.id_product, Include.quantity], callback);  
     },  
     deleteInclude: function(id, callback) {  
-        return db.query("delete from Include where id_order=?", [id], callback);  
+        return db.query("delete from Include where id_order=? && id_product=?", [id, id], callback);  
     },  
     updateInclude: function(id, Include, callback) {  
-        return db.query("update Include set quantity=? where id_order=?", [Include.quantity, id], callback);  
+        return db.query("update Include set id_order=?,id_product=?,quantity=? where id_order=? && id_product=?", [Include.id_order, Include.id_product, Include.quantity, id, id], callback);  
     }  
 };  
 module.exports = Include; 
--- models/Order.js
+++ models/Order.js
@@ -7,13 +7,13 @@ var Order = {
         return db.query("select * from Order where id_order=?", [id], callback);  
     },  
     addOrder: function(Order, callback) {  
-        return db.query("Insert into Order(order_date, order_price, id_user) values(?,?,?)", [Order.order_date, Order.order_price, Order.id_user], callback);  
+        return db.query("Insert into Order(order_date, order_price, id_user, id_status_order) values(?,?,?,?)", [Order.order_date, Order.order_price, Order.id_user, Order.id_status_order], callback);  
     },  
     deleteOrder: function(id, callback) {  
         return db.query("delete from Order where id_order=?", [id], callback);  
     },  
     updateOrder: function(id, Order, callback) {  
-        return db.query("update Order set order_date=?,order_price=?,id_user=? where id_order=?", [Order.order_date, Order.order_price, Order.id_user, id], callback);  
+        return db.query("update Order set order_date=?,order_price=?,id_user=?,id_status_order=? where id_order=?", [Order.order_date, Order.order_price, Order.id_user, Order.id_status_order, id], callback);  
     }  
 };  
 module.exports = Order;  
