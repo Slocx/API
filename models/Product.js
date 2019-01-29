58f60c913130ac272bf7424f300590fc9ec3e2ba
JeanAkaPascal <33148890+JeanAkaPascal@users.noreply.github.com>
2019-01-29 08:33:31 +0100

    need

--- models/Product.js
+++ models/Product.js
@@ -4,20 +4,19 @@ var Product = {
         return db.query("Select * from Product", callback);  
     },
     getAllProductsByOrder: function(callback){
-        return db.query("SELECT Product.id_Product,Product_name,id_order as TotOrder FROM `order` inner join product ON `order`.id_product = product.id_product GROUP BY `order`.id_product ORDER BY TotOrder DESC",callback);
+        return db.query("SELECT include.id_product, product_name, product_desc, product_price, id_category, stock, COUNT(include.id_product) as indice FROM webproject.include inner join product on include.id_product = product.id_product GROUP BY include.id_product ORDER BY indice DESC;",callback);
     },
-
     getProductsById: function(id, callback) {  
         return db.query("select * from Product where id_Product=?", [id], callback);  
     },  
     addProduct: function(Product, callback) {  
-        return db.query("Insert into Product(Product_name, Product_price, id_stock, id_category) values(?,?,?,?)", [Product.Product_name, Product.Product_body, Product.Product_date, Product.Product_location, Product.id_user_create, Product.id_status_Product, Product.id_user_validate], callback);  
+        return db.query("Insert into Product(Product_name, product_desc, Product_price, stock, id_category) values(?,?,?,?,?)", [Product.Product_name, Product.Product_desc, Product.Product_price, Product.stock, Product.id_category], callback);  
     },  
     deleteProduct: function(id, callback) {  
         return db.query("delete from Product where id_Product=?", [id], callback);  
     },  
     updateProduct: function(id, Product, callback) {  
-        return db.query("update Product set Product_name=?,Product_price=?,id_stock,id_category=? where id_Product=?", [Product.Product_name, Product.Product_price, id_stock, id_category, id], callback);  
+        return db.query("update Product set Product_name=?, Product_desc=?,Product_price=?,stock=?,id_category=? where id_Product=?", [Product.Product_name, Product.Product_desc, Product.Product_price, Product.stock, Product.id_category, id], callback);  
     }  
 };  
 module.exports = Product;  
